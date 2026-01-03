dc5feacf07513031fc8d9063eefad948c87eaa50
Linus Fischer <leicraft@leicraftmc.de>
2026-01-03 19:24:16 +0000

    fixed incorrent path for drizlle db schema

--- drizzle/utils.ts
+++ drizzle/utils.ts
@@ -3,7 +3,7 @@ import { defineConfig } from "drizzle-kit";
 export function createDrizzleConfig(dialect: 'sqlite' | 'postgresql' | 'mysql') {
     return defineConfig({
         out: `./drizzle/migrations/${dialect}`,
-        schema: './src/db/schema.ts',
+        schema: `./src/db/schema/${dialect}.ts`,
         dialect: dialect,
         dbCredentials: {
             url: process.env.DLA_DB_CONNECTION_URL!!,
